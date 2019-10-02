5f827bc4e09b002c9d735a35f60ad7ea65fc63d7
ndobryanskyy <ndobryanskyy@gmail.com>
2019-10-02 19:33:05 +0300

    Add simple client

---
+++ TechTalks.Demo.Client/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Lohika.TechTalks.News;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace TechTalks.Demo.Client
+{
+    public sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var services = new ServiceCollection();
+
+            services.AddLogging(options => { options.AddConsole(); });
+            services.AddGrpcClient<Press.PressClient>(options =>
+                {
+                    options.Address = new Uri("https://localhost:5001");
+                });
+
+            var container = services.BuildServiceProvider();
+
+            var logger = container.GetRequiredService<ILogger<Program>>();
+            var client = container.GetRequiredService<Press.PressClient>();
+
+            var response = client.GetArticleById(new GetArticleByIdRequest
+            {
+                ArticleId = 1
+            });
+
+            logger.LogInformation($"Got the article: {response.Article.Id}");
+
+            Console.ReadKey();
+        }
+    }
+}
