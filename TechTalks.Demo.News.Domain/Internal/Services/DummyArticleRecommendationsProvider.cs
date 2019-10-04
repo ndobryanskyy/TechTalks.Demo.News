4cb0065c17777963bff2ced001d55e6108746a13
ndobryanskyy <ndobryanskyy@gmail.com>
2019-10-04 14:24:32 +0300

    Make aticle recommendations provider more interesting

--- TechTalks.Demo.News.Domain/Internal/Services/DummyArticleRecommendationsProvider.cs
+++ TechTalks.Demo.News.Domain/Internal/Services/DummyArticleRecommendationsProvider.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using TechTalks.Demo.News.Domain.Abstractions;
 
@@ -10,26 +11,44 @@ namespace TechTalks.Demo.News.Domain.Internal.Services
         private readonly HashSet<int> _articlesViewed = new HashSet<int>();
 
         private readonly OnArticleRecommendationAvailable _onArticleRecommendationAvailable;
+        private readonly ILogger<DummyArticleRecommendationsProvider> _logger;
+        private readonly Random _rng;
 
-        public DummyArticleRecommendationsProvider(OnArticleRecommendationAvailable onArticleRecommendationAvailable)
+        public DummyArticleRecommendationsProvider(
+            OnArticleRecommendationAvailable onArticleRecommendationAvailable, 
+            ILogger<DummyArticleRecommendationsProvider> logger)
         {
             _onArticleRecommendationAvailable = onArticleRecommendationAvailable;
+            _logger = logger;
+            _rng = new Random();
         }
 
-        public async Task TrackViewedArticleAsync(int articleId)
+        public Task TrackViewedArticleAsync(int articleId)
         {
             if (_articlesViewed.Add(articleId))
             {
-                await Task.Delay(200);
+                var suggestionsCount = _rng.Next(1, _articlesViewed.Count);
 
-                if (_articlesViewed.Count % 3 == 0)
+                _logger.LogInformation($"Going to send {suggestionsCount} suggestions...");
+
+                for (int i = 0; i < suggestionsCount; i++)
                 {
-                    var recommendation = _articlesViewed.Max() + 1;
-                    _articlesViewed.Add(recommendation);
+                    var suggestionDelaySeconds = _rng.Next(2, 10);
 
-                    await _onArticleRecommendationAvailable(recommendation);
+                    Task.Delay(TimeSpan.FromSeconds(suggestionDelaySeconds))
+                        .ContinueWith(async t => {
+                            try
+                            {
+                                await _onArticleRecommendationAvailable(_rng.Next(1000, 100000));
+                            }
+                            catch (Exception)
+                            {
+                            }
+                        });
                 }
             }
+
+            return Task.CompletedTask;
         }
     }
 }
--- TechTalks.Demo.News.Domain/Internal/Services/NewsFactory.cs
+++ TechTalks.Demo.News.Domain/Internal/Services/NewsFactory.cs
@@ -1,4 +1,5 @@
-﻿using TechTalks.Demo.News.Domain.Abstractions;
+﻿using Microsoft.Extensions.Logging;
+using TechTalks.Demo.News.Domain.Abstractions;
 using TechTalks.Demo.News.Domain.Internal.Validation;
 using TechTalks.Demo.News.Domain.Model.Drafting;
 
@@ -6,10 +7,17 @@ namespace TechTalks.Demo.News.Domain.Internal.Services
 {
     internal sealed class NewsFactory : INewsFactory
     {
+        private readonly ILoggerFactory _loggerFactory;
+
+        public NewsFactory(ILoggerFactory loggerFactory)
+        {
+            _loggerFactory = loggerFactory;
+        }
+
         public ArticleDraft CreateArticleDraft() 
             => new ArticleDraft(Validators.ArticleDraftValidator);
 
         public IArticleRecommendationsProvider CreateArticleRecommendationsProvider(OnArticleRecommendationAvailable onArticleRecommendationAvailable)
-            => new DummyArticleRecommendationsProvider(onArticleRecommendationAvailable);
+            => new DummyArticleRecommendationsProvider(onArticleRecommendationAvailable, _loggerFactory.CreateLogger<DummyArticleRecommendationsProvider>());
     }
 }
